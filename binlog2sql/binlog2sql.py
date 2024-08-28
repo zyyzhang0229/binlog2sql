67c0dfa6c6358a6995a4d4f73cf54820fd4e2228
zhangyueyang <zhangyueyang@aisino.com>
2024-08-28 10:25:48 +0800

    cursor()

--- binlog2sql/binlog2sql.py
+++ binlog2sql/binlog2sql.py
@@ -44,7 +44,7 @@ class Binlog2sql(object):
 
         self.binlogList = []
         self.connection = pymysql.connect(**self.conn_setting)
-        with self.connection as cursor:
+        with self.connection.cursor() as cursor:
             cursor.execute("SHOW MASTER STATUS")
             self.eof_file, self.eof_pos = cursor.fetchone()[:2]
             cursor.execute("SHOW MASTER LOGS")
@@ -70,7 +70,7 @@ class Binlog2sql(object):
         e_start_pos, last_pos = stream.log_pos, stream.log_pos
         # to simplify code, we do not use flock for tmp_file.
         tmp_file = create_unique_file('%s.%s' % (self.conn_setting['host'], self.conn_setting['port']))
-        with temp_open(tmp_file, "w") as f_tmp, self.connection as cursor:
+        with temp_open(tmp_file, "w") as f_tmp, self.connection.cursor() as cursor:
             for binlog_event in stream:
                 if not self.stop_never:
                     try:
